a852ad14f133dda01f175929f819265f1b752188
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2024-11-11 10:31:14 -0300

    feat: dataset query

--- src/routes/map/dataset/+page.server.ts
+++ src/routes/map/dataset/+page.server.ts
@@ -1,5 +1,16 @@
-import type { PageServerLoad } from './$types';
+import type { PageServerLoad } from './$types'
+import { datasetC } from '$db/controller'
+import { error } from '@sveltejs/kit'
+export const load = (async ({ locals }) => {
+  const cityId = locals.user?.cityId
 
-export const load = (async () => {
-    return {};
-}) satisfies PageServerLoad;
+  if (!cityId) {
+    return error(404, 'City not found')
+  }
+
+  const cityDatasets = await datasetC.getCityDatasets(cityId)
+
+  return {
+    datasets: cityDatasets,
+  }
+}) satisfies PageServerLoad
